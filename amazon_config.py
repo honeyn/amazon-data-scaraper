f690f6f99ce57615837a6e90513d90ab4127f25b
Honey Motiyani <honey.motiyani@gmail.com>
2020-11-10 00:23:33 +0530

    working script

--- amazon_config.py
+++ amazon_config.py
@@ -2,14 +2,14 @@ from selenium import webdriver
 
 DIRECTORY = 'reports'
 NAME = 'PS4'
-CURRENCY = '€'
-MIN_PRICE = '275'
-MAX_PRICE = '650'
+CURRENCY = '₹'
+MIN_PRICE = '20000'
+MAX_PRICE = '30000'
 FILTERS = {
     'min': MIN_PRICE,
     'max': MAX_PRICE
 }
-BASE_URL = "http://www.amazon.de/"
+BASE_URL = "http://www.amazon.in/"
 
 
 def get_chrome_web_driver(options):
